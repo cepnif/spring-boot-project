e4129c781be8492a36759bfa48f4e55134d0be39
cepnif <fatih@fincityltd.com>
2021-03-10 17:08:56 +0000

    path variable added

--- spring-13-spring-boot-mvc-controller/src/main/java/com/fincity/FincityApplication.java
+++ spring-13-spring-boot-mvc-controller/src/main/java/com/fincity/FincityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FincityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FincityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FincityApplication.class, args);
+    }
 
 }
--- spring-13-spring-boot-mvc-controller/src/main/java/com/fincity/controller/HomeController.java
+++ spring-13-spring-boot-mvc-controller/src/main/java/com/fincity/controller/HomeController.java
@@ -1,2 +1,35 @@
-package com.fincity.controller;public class HomeController {
+package com.fincity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String getRequestMapping1(){
+        return "home";
+    }
+
+    @GetMapping("/home")
+    public String getRequestMapping2(){
+        return "home";
+    }
+
+    @GetMapping("/fincity")
+    public String getRequestMapping3(){
+        return "home";
+    }
+
+    @GetMapping("/home/{name}/{id}")
+    public String getRequestMapping4(@PathVariable("name") String name, @PathVariable("id") String id){
+        System.out.println("Name is " + name);
+        System.out.println("Id is " + id);
+        return "home";
+    }
+
+
 }
