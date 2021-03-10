0f5f2c6c9cea4404dddce29a61bb5943c7050b78
cepnif <fatih@fincityltd.com>
2021-03-10 16:33:52 +0000

    mvc controller get mappping added

---
+++ spring-13-spring-boot-mvc-controller/src/main/java/com/fincity/FincityApplication.java
@@ -0,0 +1,13 @@
+package com.fincity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincityApplication.class, args);
+	}
+
+}
---
+++ spring-13-spring-boot-mvc-controller/src/main/java/com/fincity/controller/HomeController.java
@@ -0,0 +1,2 @@
+package com.fincity.controller;public class HomeController {
+}
